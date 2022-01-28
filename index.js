421de0b2a63a3ecc0ac135fed483e25915efe5a3
Richnei <rich.nei@hotmail.com>
2022-01-28 19:55:01 -0300

    users

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const app = express();
 const path = require("path");
 const socketIo = require("socket.io");
 
-app.use("/", express.static(path.join(__dirname, "public")));
+app.use("/", express.static(path.join(__dirname, "/public")));
 
 const server = app.listen(3000, () => {
   console.log("Running");
@@ -17,7 +17,7 @@ io.on("connection", (socket) => {
   socket.emit("update_messages", messages);
 
   socket.on("new_message", (data) => {
-    messages.push(data.msg);
+    messages.push(data);
     console.log(messages);
     io.emit("update_messages", messages);
   });
